763441682905bf35ee169c054fbf0f7992a0e3a4
rishulpu1 <rishulpu1@gmail.com>
2024-07-03 17:59:45 +0530

    setting up router and controller

---
+++ app/Http/Controllers/ExampleCOntroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleCOntroller extends Controller
+{
+    public function homePage(){
+        return view("homepage");
+    }
+    public function aboutPage(){
+        return "<h1>About Page</h1><a href='/'>Go to Home Page</a>";
+    }
+}
