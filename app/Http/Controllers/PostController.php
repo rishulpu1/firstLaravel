ad2cc822f30c04effe3079cb2095e93b4ca89bbd
Rajneesh Kumar <rishulpu1@gmail.com>
2024-07-10 22:36:36 +0530

    created single blog post and redirected to it after creating

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
+    public function viewSinglePost(Post $post){
+        return view('single-post', ["post" => $post]);
+    }
     public function storeNewPost(Request $request){
         $incomingFields = $request->validate([
             'title'=>'required',
@@ -16,8 +19,8 @@ class PostController extends Controller
         $incomingFields['title'] = strip_tags($incomingFields['title']);
         $incomingFields['body'] = strip_tags($incomingFields['body']);
         $incomingFields['user_id'] = auth()->id();
-        Post::create($incomingFields);
-        return "Hey!";
+        $newPost = Post::create($incomingFields);
+        return redirect("/post/{$newPost->id}")->with('success', 'New post added successfully!!');
     }
     public function showCreateForm(){
         return view('create-post');
